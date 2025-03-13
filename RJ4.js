9ff5c58e736ca55c7eb7355e45e18ab06e881475
Julia <165340522+juliaroosas@users.noreply.github.com>
2025-03-13 00:40:40 -0300

    Update RJ4.js

--- RJ4.js
+++ RJ4.js
@@ -1,134 +1,126 @@
-"Definição das classes"
-
 class Telefone {
-  constructor(numero) {
-      this.numero = numero;
-  }
+    constructor(ddd, numero) {
+        this.ddd = ddd;
+        this.numero = numero;
+    }
 
-  getNumero() {
-      return this.numero;
-  }
+    getNumero() {
+        return this.ddd + " " + this.numero;
+    }
 
-  getNumeroMaiusculo() {
-      return this.numero.toUpperCase();
-  }
+    getNumeroMaiusculo() {
+        return this.getNumero().toUpperCase();
+    }
 
-  getNumeroMinusculo() {
-      return this.numero.toLowerCase();
-  }
+    getNumeroMinusculo() {
+        return this.getNumero().toLowerCase();
+    }
 }
 
 class Endereco {
-  constructor(rua, numero, cidade) {
-      this.rua = rua;
-      this.numero = numero;
-      this.cidade = cidade;
-  }
+    constructor(estado, cidade, rua, numero) {
+        this.estado = estado;
+        this.cidade = cidade;
+        this.rua = rua;
+        this.numero = numero;
+    }
 
-  getEndereco() {
-      return this.rua + ", " + this.numero + ", " + this.cidade;
-  }
+    getEndereco() {
+        return "Estado: " + this.estado + " Cidade: " + this.cidade + " Rua: " + this.rua + " Número: " + this.numero;
+    }
 
-  getEnderecoMaiusculo() {
-      return this.getEndereco().toUpperCase();
-  }
+    getEnderecoMaiusculo() {
+        return this.getEndereco().toUpperCase();
+    }
 
-  getEnderecoMinusculo() {
-      return this.getEndereco().toLowerCase();
-  }
+    getEnderecoMinusculo() {
+        return this.getEndereco().toLowerCase();
+    }
 }
 
 class Cliente {
-  #cpf;
+    #cpf;
 
-  constructor(nome, cpf, endereco) {
-      this.nome = nome;
-      this.#cpf = cpf;
-      this.endereco = endereco;
-      this.telefones = [];
-  }
+    constructor(nome, cpf, endereco) {
+        this.nome = nome;
+        this.#cpf = cpf;
+        this.endereco = endereco;
+        this.telefones = [];
+    }
 
-  getCpf() {
-      return this.#cpf;
-  }
+    getCpf() {
+        return this.#cpf;
+    }
 
-  getCpfMaiusculo() {
-      return this.#cpf.toUpperCase();
-  }
+    getCpfMaiusculo() {
+        return this.#cpf.toUpperCase();
+    }
 
-  getCpfMinusculo() {
-      return this.#cpf.toLowerCase();
-  }
+    getCpfMinusculo() {
+        return this.#cpf.toLowerCase();
+    }
 
-  adicionarTelefone(telefone) {
-      this.telefones.push(telefone);
-  }
+    adicionarTelefone(telefone) {
+        this.telefones.push(telefone);
+    }
+
+    removerTelefone(numero) {
+        this.telefones = this.telefones.filter(tel => tel.getNumero() !== numero);
+    }
+
+    getDetalheCliente() {
+        let detalhe = "Nome: " + this.nome.toUpperCase() + "\n";
+        detalhe += this.endereco.getEndereco() + "\n";
+        this.telefones.forEach(tel => {
+            detalhe += "ddd: " + tel.ddd + " número: " + tel.numero + "\n";
+        });
+        return detalhe;
+    }
 }
 
 class Empresa {
-  #cnpj;
+    #cnpj;
 
-  constructor(nome, cnpj, endereco) {
-      this.nome = nome;
-      this.#cnpj = cnpj;
-      this.endereco = endereco;
-      this.clientes = [];
-      this.telefones = [];
-  }
+    constructor(razaoSocial, nomeFantasia, cnpj, endereco) {
+        this.razaoSocial = razaoSocial;
+        this.nomeFantasia = nomeFantasia;
+        this.#cnpj = cnpj;
+        this.endereco = endereco;
+        this.clientes = [];
+        this.telefones = [];
+    }
 
-  getCnpj() {
-      return this.#cnpj;
-  }
+    getCnpj() {
+        return this.#cnpj;
+    }
 
-  getCnpjMaiusculo() {
-      return this.#cnpj.toUpperCase();
-  }
+    getCnpjMaiusculo() {
+        return this.#cnpj.toUpperCase();
+    }
 
-  getCnpjMinusculo() {
-      return this.#cnpj.toLowerCase();
-  }
+    getCnpjMinusculo() {
+        return this.#cnpj.toLowerCase();
+    }
 
-  adicionarCliente(cliente) {
-      this.clientes.push(cliente);
-  }
+    adicionarCliente(cliente) {
+        this.clientes.push(cliente);
+    }
 
-  adicionarTelefone(telefone) {
-      this.telefones.push(telefone);
-  }
+    adicionarTelefone(telefone) {
+        this.telefones.push(telefone);
+    }
 
-  gerarDescricao() {
-      console.log("Empresa: " + this.nome + " (CNPJ: " + this.getCnpj() + ")");
-      console.log("Endereço: " + this.endereco.getEndereco());
-      console.log("Telefones: " + this.telefones.map(t => t.getNumero()).join(", "));
-      console.log("\nClientes:");
+    detalhe() {
+        let descricao = "Razão Social: " + this.razaoSocial + "\n";
+        descricao += "Nome fantasia: " + this.nomeFantasia + "\n";
+        descricao += "-----------------\n";
 
-      this.clientes.forEach(cliente => {
-          console.log("- " + cliente.nome + " (CPF: " + cliente.getCpf() + ")");
-          console.log("  Endereço: " + cliente.endereco.getEndereco());
-          console.log("  Telefones: " + cliente.telefones.map(t => t.getNumero()).join(", "));
-          console.log("");
-      });
-  }
+        this.clientes.forEach(cliente => {
+            descricao += cliente.getDetalheCliente() + "\n";
+        });
+
+        return descricao;
+    }
 }
 
-"Criação da empresa"
-
-const enderecoEmpresa = new Endereco("Rua Bacabal", 123, "São José dos Campos");
-const empresa = new Empresa("Orbis", "12.345.678/0001-99", enderecoEmpresa);
-empresa.adicionarTelefone(new Telefone("12-99999-8888"));
-empresa.adicionarTelefone(new Telefone("12-98888-7777"));
-
-"Criação de clientes"
-
-const cliente1 = new Cliente("Ana", "111.222.333-44", new Endereco("Rua da Bromélias", 10, "São José dos Campos"));
-cliente1.adicionarTelefone(new Telefone("12-91111-2222"));
-cliente1.adicionarTelefone(new Telefone("12-92222-3333"));
-
-const cliente2 = new Cliente("Bruno", "222.333.444-55", new Endereco("Rua das Azaleias", 20, "Taubaté"));
-cliente2.adicionarTelefone(new Telefone("12-94444-5555"));
-cliente2.adicionarTelefone(new Telefone("12-95555-6666"));
-
-empresa.adicionarCliente(cliente1);
-empresa.adicionarCliente(cliente2);
-
-empresa.gerarDescricao();
+export { Telefone, Endereco, Cliente, Empresa };
